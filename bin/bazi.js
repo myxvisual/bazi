97f2075feeeff235fc7a51efdfce5a418a0d8160
myxvisual <myxvisual@live.com>
2018-08-03 10:59:51 +0800

    release: Cut the v0.0.2 version

--- bin/bazi.js
+++ bin/bazi.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var fs = require('fs')
+var fse = require('fs-extra')
 var execSync = require('child_process').execSync
 var exec = require('child_process').exec
 var chalk = require('chalk')
@@ -21,17 +22,18 @@ program
   .version('0.1.0', '-v, --version')
   .arguments('<project-directory>')
   .usage(`${chalk.green('<project-directory>')} [options]`)
-  .option('-s, --setup-repository <repositoryUrl>', 'setup custom repository', setupRepository)
+  .option('-s, --setup-repository <repositoryUrl>', 'setup custom repository', setupRepositoryUrl)
   .option('-u, --update-repository <repositoryName>', 'update custom repository by folder name', updateRepository)
-  .option('-r, --replace-keyword <replaceKeyword>', 'set replace keyword to template', setReplaceKeyword)
-  .option('-l, --ls', 'list the repositories', listTemplate)
+  .option('-k, --keyword-to-replace <keyword>', 'set replace keyword to repositorys', setKeyword)
+  .option('-l, --ls', 'list the repositories', listRepositorys)
   .option('-d, --default', 'set config to default', setConfigDefault)
   .action(name => {
     projectName = name
-    console.log(name)
     init()
+    cloneRepositroyFolder()
   })
-
+  
+console.log(chalk.green(`current setup repository is: ${config.setupRepository}`))
 program.parse(argv)
 
 function init() {
@@ -40,42 +42,103 @@ function init() {
     repositoryUrls.forEach(repositoryUrl => {
       var repositoryName = repositoryUrl.match(repositoryReg)[1]
       if (repositoryName) {
-        var isExist = fs.existsSync(`../template/${repositoryName}`)
+        var isExist = fs.existsSync(`../repositorys/${repositoryName}`)
         if (isExist) {
           // console.log(`repository: ${repositoryName} is existed.`)
         } else {
           console.log(chalk.red(`repository: ${repositoryName} dosen't exist`))
           console.log(chalk.green(`clone repository ${repositoryName}...` ))
-          execSync(`cd ../template && git clone ${repositoryUrl}`)
+          execSync(`cd ../repositorys && git clone ${repositoryUrl}`)
         }
       }
     })
   }
 }
 
-function setupRepository(repositoryUrl) {
-  execSync(`cd ../template && git clone ${repositoryUrl}`)
-}
-
-function updateRepository(repositoryName) {
-  if (fs.existsSync(`../template/${repositoryName}`)) {
-    execSync(`cd ../template/${repositoryName} && git pull`)
-  } else {
-    console.log(chalk.red(`repository: ${repositoryName} dosen't exist`))
-    if (config.repositoryUrls.some(repositoryUrl => repositoryUrl.includes(repositoryName))) {
-      execSync(`cd ../template && git clone ${repositoryUrl}`)
+function cloneRepositroyFolder() {
+  if (projectName) {
+    var repositoryDir = `../repositorys/${config.setupRepository}`
+    var ouputDir =  path.join(currentPath, projectName)
+    if (fs.existsSync(repositoryDir)) {
+      fse.copySync(repositoryDir, ouputDir)
+      execSync(`rm -rf ${ouputDir}/.git`)
+      replaceFolderKeyword(ouputDir)
     }
   }
 }
 
-function setReplaceKeyword(replaceKeyword) {
-  config.replaceKeyword = replaceKeyword
-  writeCachedConfigfile()
-  console.log(chalk.green(`set replaceKeyword: ${replaceKeyword}`))
+function replaceFolderKeyword(originDir) {
+  var files = fs.readdirSync(originDir)
+  var reg = new RegExp(config.setupRepository, 'g')
+  files.forEach(fileOrDir => {
+    var currFile = path.join(originDir, fileOrDir)
+    var isDirectory = fs.lstatSync(currFile).isDirectory()
+    var newFile
+    if (fileOrDir.includes(config.replaceKeyword)) {
+      newFile = currFile.replace(reg, projectName)
+      fs.renameSync(currFile, newFile)
+    }
+    if (isDirectory) {
+      replaceFolderKeyword(newFile || currFile)
+    } else {
+      var content = fs.readFileSync(newFile || currFile).toString()
+      if (content.includes(config.replaceKeyword)) {
+        content = content.replace(reg, projectName)
+        fs.writeFileSync(newFile || currFile, content, 'utf-8')
+      }
+    }
+  })
 }
 
-function listTemplate() {
-  console.log(chalk.green(`repositoryUrls is:\n${config.repositoryUrls}`))
+function setupRepositoryUrl(repositoryUrlOrName) {
+  var repositoryName
+  var isGitUrl = repositoryReg.test(repositoryUrlOrName)
+  if (isGitUrl) {
+    repositoryName = repositoryUrlOrName.match(repositoryReg)[1]
+    if (repositoryName) {
+      if (fs.existsSync(`../repositorys/${repositoryName}`)) {
+        execSync('../repositorys && git pull')
+      } else {
+        execSync(`cd ../repositorys && git clone ${repositoryUrlOrName}`)
+      }
+    }
+  } else {
+    config.repositoryUrls.forEach(repositoryUrl => {
+      var name = repositoryUrl.match(repositoryReg)[1]
+      if (name === repositoryUrlOrName) {
+        repositoryName = repositoryUrlOrName
+      }
+    })
+  }
+  if (repositoryName) {
+    config.setupRepository = repositoryName
+    if (isGitUrl && !config.repositoryUrls.includes(repositoryUrlOrName)) {
+      config.repositoryUrls.push(repositoryUrlOrName)
+    }
+    console.log(chalk.green(`setup repositoryName: ${repositoryName}`))
+    writeCachedConfigfile()
+  }
+}
+
+function updateRepository(repositoryName) {
+  if (fs.existsSync(`../repositorys/${repositoryName}`)) {
+    execSync(`cd ../repositorys/${repositoryName} && git pull`)
+  } else {
+    console.log(chalk.red(`repository: ${repositoryName} dosen't exist`))
+    if (config.repositoryUrls.some(repositoryUrl => repositoryUrl.includes(repositoryName))) {
+      execSync(`cd ../repositorys && git clone ${repositoryUrl}`)
+    }
+  }
+}
+
+function setKeyword(keyword) {
+  config.replaceKeyword = keyword
+  writeCachedConfigfile()
+  console.log(chalk.green(`set keyword: ${keyword}`))
+}
+
+function listRepositorys() {
+  console.log(chalk.green(`repositoryUrls is:\n${JSON.stringify(config.repositoryUrls, null, 2)}`))
 }
 
 function setConfigDefault() {
@@ -87,5 +150,5 @@ function setConfigDefault() {
 }
 
 function writeCachedConfigfile() {
-  fs.writeFileSync(`./${chachedConfigFile}`, JSON.stringify(config, null, 2))
+  fs.writeFileSync(`${chachedConfigFile}`, JSON.stringify(config, null, 2))
 }
